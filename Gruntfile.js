25924e488fff1771f4c559c650c3f45bf184bade
Jan Voráček <jan@voracek.net>
2014-10-08 15:30:50 +0200

    Added cssmin grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,19 @@ module.exports = function(grunt) {
                     basePath: '_sass'
                 }
             }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'css/main.min.css': ['css/main.css']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['compass']);
+    grunt.registerTask('default', ['compass', 'cssmin']);
 
 };
